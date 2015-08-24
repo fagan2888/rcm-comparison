12127052af04170debec54b4560395ed4aabce29
jpraymond <raymonjp@iro.umontreal.ca>
2015-08-24 08:53:42 -0400

    ...

--- computingPSTailLoss.m
+++ computingPSTailLoss.m
@@ -2,10 +2,9 @@
 % =========================================================
 
 
-% ESTIMATED_BETAS = [-2.7629, -0.9894, -0.5637, -4.3165, 1.5382]; % 2015
-ESTIMATED_BETAS = [-2.7538, -0.9936, -0.5638, -4.4168, 1.5303]; % 2015-0 
+ESTIMATED_BETAS = [-2.7629, -0.9894, -0.5637, -4.3165, 1.5382]; % 2015
 
-N_DRAWS = 500;
+N_DRAWS = 5;
 
 % Seed used for the partitionning of the observations into three sets (training,
 % validation and test).
@@ -33,13 +32,13 @@ validSet = myObs(idxEndTrain+1:idxEndValid, :);
 testSet = myObs(idxEndValid+1:end, :);
 
 
-valid = pathGeneration(validSet, ...
-                       sprintf('valid%d', RNG_SEED), ...
-                       N_DRAWS, ...
-                       ESTIMATED_BETAS, ...
-                       false, ...
-                       'rngSeed', 20155);
-paths = getPaths(valid);
+valid5 = pathGeneration(validSet, ...
+                        sprintf('valid%d', RNG_SEED), ...
+                        N_DRAWS, ...
+                        ESTIMATED_BETAS, ...
+                        false, ...
+                        'rngSeed', 20155);
+paths = getPaths(valid5);
 
 % TODO: Most of the following is way too involved to be in a user script. It
 %       should be put in one or several functions (in /project_code).
